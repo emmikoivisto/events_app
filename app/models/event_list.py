8d5d76ecc88ebcf5d2b71fe24c71730b17fb1099
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-09-24 16:14:19 +0100

    this works! can add event to list MVP

--- app/models/event_list.py
+++ app/models/event_list.py
@@ -4,5 +4,5 @@ event_1 = Event("2020-03-16", "Slumber party", 6, "Campus", "Party time!")
 event_2 = Event("2020-01-28", "Concert", 128, "O2 Arena", "My favourite pop band")
 events = [event_1, event_2]
 
-def add_event(event):
-    events.append(event)
+def add_new_event(new_event):
+    events.append(new_event)
