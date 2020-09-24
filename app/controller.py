2756dd8798bedb5c237b752ddc31cb859e8752b1
Mathew-Tomaney <mat.tomaney@gmail.com>
2020-09-24 15:42:58 +0100

    found two bugs
    extends base.html was missing quotations
    were importing Flask instead of flask so it wasn't working. resolved.

--- app/controller.py
+++ app/controller.py
@@ -1,9 +1,9 @@
 from app import app
-from Flask import render_template
+from flask import render_template
 from app.models.event_list import events, add_event
 from app.models.event import *
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home', events=events)
+    return render_template('index.html', title='Home')
 
